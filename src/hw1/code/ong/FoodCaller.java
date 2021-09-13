7ffc58a58f271892e2ab3ffdfe1004c628d481ba
Jowyl Ong <jowylong@gmail.com>
2021-09-12 20:05:35 -0700

    Added FoodCaller to print a food sentence.

---
+++ src/hw1/code/ong/FoodCaller.java
@@ -0,0 +1,10 @@
+package hw1.code.ong;
+
+public class FoodCaller {
+
+    public static void main(String[] args) {
+        Food food1 = new Food();
+        food1.setFood("sushi");
+        food1.sayFood();
+    }
+}
