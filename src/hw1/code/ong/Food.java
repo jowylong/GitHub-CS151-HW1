7a6151c324b4364239df7a7b0a57f0b7c859e2ce
Jowyl Ong <jowylong@gmail.com>
2021-09-12 19:54:44 -0700

    Added Food class with instance variables and constructor.

---
+++ src/hw1/code/ong/Food.java
@@ -0,0 +1,14 @@
+package hw1.code.ong;
+
+public class Food {
+
+    private String food;
+
+    public void setFood(String newFood) {
+        food = newFood;
+    }
+
+    public void sayFood(){
+        System.out.println("My favorite food is " + food + "!");
+    }
+}
