35c18dcda13e22868c73c7cd81f3804a8d315ead
Jowyl Ong <jowylong@gmail.com>
2021-09-12 20:11:38 -0700

    Added javadoc that helps explain what each class does.

--- src/hw1/code/ong/Food.java
+++ src/hw1/code/ong/Food.java
@@ -1,13 +1,26 @@
 package hw1.code.ong;
 
+/**
+ * Food class that initializes the food name and sentence.
+ */
 public class Food {
 
+    /**
+     * Food variable.
+     */
     private String food;
 
+    /**
+     * Initializes the food name.
+     * @param newFood Name of the food
+     */
     public void setFood(String newFood) {
         food = newFood;
     }
 
+    /**
+     * Prints a sentence that involves the food name.
+     */
     public void sayFood(){
         System.out.println("My favorite food is " + food + "!");
     }
--- src/hw1/code/ong/FoodCaller.java
+++ src/hw1/code/ong/FoodCaller.java
@@ -1,7 +1,14 @@
 package hw1.code.ong;
 
+/**
+ * FoodCaller class that prints out the food sentence with a food name.
+ */
 public class FoodCaller {
 
+    /**
+     * Adds the name of the food then prints out the favorite food sentence.
+     * @param args Not used
+     */
     public static void main(String[] args) {
         Food food1 = new Food();
         food1.setFood("sushi");
